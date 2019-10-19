02df893f9fb7b7bcee83842eb4015e8216c56402
Chris <christopher.vassallo@uconn.edu>
2019-10-19 15:38:45 -0400

    no

--- Socket/app.js
+++ Socket/app.js
@@ -5,12 +5,13 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 8080;
 
+
 //room id creator
 var ID = function () {
   // Math.random should be unique because of its seeding algorithm.
   // Convert it to base 36 (numbers + letters), and grab the first 9 characters
   // after the decimal.
-  return '_' + Math.random().toString(36).substr(2, 9);
+  return Math.random().toString(36).substr(2, 4);
 };
 
 //Start server
@@ -27,30 +28,45 @@ app.get('/', (req, res) => {
 */
 
 // room ids
-let rooms = [];
-function room(roomId, host, clients) {
-  this.id = roomId || "";
-  this.host = host || "";
+let rooms = {};
+function room(host, clients) {
+  this.host = host;
   this.clients = clients || [];
 }
 
 io.on('connection', (socket) => {
-  console.log("Client "+socket.id+" has joined!");
+
 
   //handle host joining
   socket.on('host join', () => {
     socket.host = true;
-    let roomId = ID();
 
-    rooms.push(new room(roomID, socket.id));
+    //create room
+    let roomId = ID();
+    socket.roomId = roomId;
+    rooms[roomId] = new room(socket);
+    socket.room = rooms[roomId];
+    socket.emit('host room info', {roomId: roomId, clientLength: socket.room.clients.length });
+    console.log("Host " + socket.id + " has joined and created room " + roomId);
   });
 
   //handle client joining
-  socket.on('client join', () => {
+  socket.on('client join', (roomId) => {
     socket.host = false;
+    socket.roomId = roomId;
+    socket.room = rooms[roomId];
+    rooms[roomId].clients.push(socket);
+    socket.room.host.emit('host room info', {roomId: roomId, clientLength: socket.room.clients.length });
+    console.log("Client " + socket.id + " has joined!");
   });
 
   socket.on('disconnect', (reason) => {
-    console.log("Client "+socket.id+" has left, because of '" + reason +"'.");
+    console.log((socket.host) ? "Host " + socket.id + " has left, because of '" + reason + "'." : "Client " + socket.id + " has left, because of '" + reason + "'.");
+
+    //delete room
+    if (socket.host) {
+      delete rooms[socket.roomId];
+      // TODO: have clients timeout when room deleted
+    }
   });
 });
