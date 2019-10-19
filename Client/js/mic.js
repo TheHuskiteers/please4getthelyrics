588fd8a23e34b19cc4620da331197b0150c435e3
Jacob Dillon <jacob.dillon@uconn.edu>
2019-10-19 15:35:35 -0400

    Did some linting

--- Client/js/mic.js
+++ Client/js/mic.js
@@ -1,53 +1,51 @@
 try {
-    var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    var recognition = new SpeechRecognition();
-  }
-  catch(e) {
-    console.error(e);
-    $('.no-browser-support').show();
-    $('.app').hide();
-  }
+  var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+  var recognition = new SpeechRecognition()
+} catch (e) {
+  console.error(e)
+  $('.no-browser-support').show()
+  $('.app').hide()
+}
 
-  recognition.onstart = function() { 
-    instructions.text('Voice recognition activated. Try speaking into the microphone.');
-  }
-  
-  recognition.onspeechend = function() {
-    instructions.text('You were quiet for a while so voice recognition turned itself off.');
-  }
-  
-  recognition.onerror = function(event) {
-    if(event.error == 'no-speech') {
-      instructions.text('No speech was detected. Try again.');  
-    };
-  }
+recognition.onstart = function () {
+  instructions.text('Voice recognition activated. Try speaking into the microphone.')
+}
 
-  recognition.onresult = function(event) {
+recognition.onspeechend = function () {
+  instructions.text('You were quiet for a while so voice recognition turned itself off.')
+}
 
-    // event is a SpeechRecognitionEvent object.
-    // It holds all the lines we have captured so far. 
-    // We only need the current one.
-    var current = event.resultIndex;
-  
-    // Get a transcript of what was said.
-    var transcript = event.results[current][0].transcript;
-  
-    // Add the current transcript to the contents of our Note.
-    noteContent += transcript;
-    noteTextarea.val(noteContent);
-  }
+recognition.onerror = function (event) {
+  if (event.error == 'no-speech') {
+    instructions.text('No speech was detected. Try again.')
+  };
+}
 
-var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+recognition.onresult = function (event) {
+  // event is a SpeechRecognitionEvent object.
+  // It holds all the lines we have captured so far.
+  // We only need the current one.
+  var current = event.resultIndex
 
-  if(!mobileRepeatBug) {
-    noteContent += transcript;
-    noteTextarea.val(noteContent);
-  }
+  // Get a transcript of what was said.
+  var transcript = event.results[current][0].transcript
 
-  $('#start-record-btn').on('click', function(e) {
-    recognition.start();
-  });
+  // Add the current transcript to the contents of our Note.
+  noteContent += transcript
+  noteTextarea.val(noteContent)
+}
 
-  $('#pause-record-btn').on('click', function(e) {
-    recognition.stop();
-  });
+var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript)
+
+if (!mobileRepeatBug) {
+  noteContent += transcript
+  noteTextarea.val(noteContent)
+}
+
+$('#start-record-btn').on('click', function (e) {
+  recognition.start()
+})
+
+$('#pause-record-btn').on('click', function (e) {
+  recognition.stop()
+})
--- Socket/app.js
+++ Socket/app.js
@@ -1,25 +1,25 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 8080;
+var express = require('express')
+var app = express()
+var path = require('path')
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
+var port = process.env.PORT || 8080
 
-//room id creator
+// room id creator
 var ID = function () {
   // Math.random should be unique because of its seeding algorithm.
   // Convert it to base 36 (numbers + letters), and grab the first 9 characters
   // after the decimal.
-  return '_' + Math.random().toString(36).substr(2, 9);
-};
+  return '_' + Math.random().toString(36).substr(2, 9)
+}
 
-//Start server
+// Start server
 server.listen(port, () => {
-  console.log('Server listening at port %d', port);
-});
+  console.log('Server listening at port %d', port)
+})
 
 // Routing
-app.use(express.static(path.join(__dirname, 'html')));
+app.use(express.static(path.join(__dirname, 'html')))
 /*
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/host');
@@ -27,30 +27,30 @@ app.get('/', (req, res) => {
 */
 
 // room ids
-let rooms = [];
-function room(roomId, host, clients) {
-  this.id = roomId || "";
-  this.host = host || "";
-  this.clients = clients || [];
+const rooms = []
+function room (roomId, host, clients) {
+  this.id = roomId || ''
+  this.host = host || ''
+  this.clients = clients || []
 }
 
 io.on('connection', (socket) => {
-  console.log("Client "+socket.id+" has joined!");
+  console.log('Client ' + socket.id + ' has joined!')
 
-  //handle host joining
+  // handle host joining
   socket.on('host join', () => {
-    socket.host = true;
-    let roomId = ID();
+    socket.host = true
+    const roomId = ID()
 
-    rooms.push(new room(roomID, socket.id));
-  });
+    rooms.push(new room(roomID, socket.id))
+  })
 
-  //handle client joining
+  // handle client joining
   socket.on('client join', () => {
-    socket.host = false;
-  });
+    socket.host = false
+  })
 
   socket.on('disconnect', (reason) => {
-    console.log("Client "+socket.id+" has left, because of '" + reason +"'.");
-  });
-});
+    console.log('Client ' + socket.id + " has left, because of '" + reason + "'.")
+  })
+})
