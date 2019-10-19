bbd444cf1f32f0843aa49f054050f18554d398e3
Xuan Chau <xuan.chau@uconn.edu>
2019-10-19 19:07:18 -0400

    irrelevant stuff

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express')
 var app = express()
 var SpotifyWebApi = require('spotify-web-api-node')
 var cookieParser = require('cookie-parser')
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
 
 app.use(cookieParser())
 
@@ -10,6 +12,7 @@ app.use(express.static(path.join(__dirname, '/public')))
 
 const port = 3000
 
+//// Logic for Spotify Web API (authenticate user, play/pause)
 var spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
@@ -20,10 +23,6 @@ const scopes = ['streaming', 'user-modify-playback-state']
 
 app.get('/', (req, res) => {
   res.redirect('/game.html')
-})
-
-app.get('/join/:id', (req, res) => {
-    // Socket logic to join room goes here
 })
 
 app.get('/togglePlay', (req, res) => {
@@ -46,4 +45,28 @@ app.get('/callback', (req, res) => {
   }).catch((err) => console.log('Yikes! ' + err.message))
 })
 
+//// Logic for socket namespace/host/client management
+
+// room id creator
+var ID = function () {
+  return Math.random().toString(36).substr(2, 4)
+}
+
+// room ids
+const rooms = {}
+function Room (host, clients, difficulty) {
+  this.host = host
+  this.clients = clients || []
+  // keep track of game status
+  this.difficulty = difficulty || 'easy'
+  this.songs = [] // lst:str, will be chosen once clients are set in stone, there should be c * (# players) songs
+  this.cur // current song
+}
+
+app.get('/join/:id', (req, res) => {
+  // Socket logic to join room goes here
+  
+})
+
+// start server
 app.listen(port, () => console.log('please4getthelyrics listening on port ' + port))
