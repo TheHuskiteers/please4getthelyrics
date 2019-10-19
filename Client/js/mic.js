9c9ce95c2908115243c15cb2aebaf7f9065b7e17
Michael Taylor <47544533+mike-taylor99@users.noreply.github.com>
2019-10-19 19:18:12 -0400

    remove loading dots after audio processing

--- Client/js/mic.js
+++ Client/js/mic.js
@@ -1,19 +1,23 @@
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-    let finalTranscript = '';
-    let recognition = new window.SpeechRecognition();
-    recognition.interimResults = true;
-    recognition.maxAlternatives = 10;
-    recognition.continuous = false;
-    recognition.onresult = (event) => {
-      let interimTranscript = '';
-      for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
-        let transcript = event.results[i][0].transcript;
-        if (event.results[i].isFinal) {
-          finalTranscript += transcript;
-        } else {
-          interimTranscript += transcript;
-        }
-      }
-      document.querySelector('p').innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
-    }
-    recognition.start();
+   let finalTranscript = '';
+   let recognition = new window.SpeechRecognition();
+   recognition.interimResults = true;
+   recognition.maxAlternatives = 10;
+   recognition.continuous = false;
+   recognition.onresult = (event) => {
+     let interimTranscript = '';
+     for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+       let transcript = event.results[i][0].transcript;
+       if (event.results[i].isFinal) {
+         finalTranscript += transcript;
+       } else {
+         interimTranscript += transcript;
+       }
+     }
+     document.querySelector('p').innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
+   }
+   recognition.start();
+   recognition.onspeechend = function() {
+     document.getElementById("load").className = document.getElementById("load").className.replace(/\bloading\b/,'');
+     recognition.stop();
+   }
