078f2467ca9851434b8b845ea004893d5af5b58a
brandonmino <43628774+brandonmino@users.noreply.github.com>
2019-10-20 01:28:28 -0400

    finally got the mic to work wooooo

--- Client/js/countdown.js
+++ Client/js/countdown.js
@@ -11,38 +11,39 @@ function timer () {
 };
 
 function mic() {
-    document.getElementById("load").className = document.getElementById("load").className.replace('',/\bloading\b/);
-    document.getElementById("aura").style.animationName = "true";
-    document.getElementById("aura").style.background  = "radial-gradient(#ffffff, transparent)";
-
-    window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-    let finalTranscript = '';
-    let recognition = new window.SpeechRecognition();
-    recognition.interimResults = true;
-    recognition.maxAlternatives = 10;
-    recognition.continuous = false;
-    recognition.onresult = (event) => {
+    setTimeout(function() {
+        document.getElementById("load").innerHTML = "Sing!"
         document.getElementById("load").className = document.getElementById("load").className.replace('',/\bloading\b/);
-        document.getElementById("aura").style.animationName = "true";
-        document.getElementById("aura").style.background  = "radial-gradient(#ffffff, transparent)";
-        let interimTranscript = '';
-        for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
-        let transcript = event.results[i][0].transcript;
-        if (event.results[i].isFinal) {
-            finalTranscript += transcript;
-        } else {
-            interimTranscript += transcript;
+        document.documentElement.style.setProperty('--key-color', '#ff9900');
+
+        window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+        let finalTranscript = '';
+        let recognition = new window.SpeechRecognition();
+        recognition.interimResults = true;
+        recognition.maxAlternatives = 10;
+        recognition.continuous = false;
+        recognition.onresult = (event) => {
+            document.documentElement.style.setProperty('--key-color', '#ff9900');
+            document.getElementById("load").className = document.getElementById("load").className.replace('',/\bloading\b/);
+            let interimTranscript = '';
+            for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+            let transcript = event.results[i][0].transcript;
+            if (event.results[i].isFinal) {
+                finalTranscript += transcript;
+            } else {
+                interimTranscript += transcript;
+            }
+            }
+            document.querySelector('p').innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
         }
+        recognition.start();
+        recognition.onspeechend = function() {
+            document.getElementById("load").className = document.getElementById("load").className.replace(/\bloading\b/,'');
+            document.getElementById("aura").style.animationName = "false";
+            document.documentElement.style.setProperty('--key-color', '#e6e6e6');
+            recognition.stop();
         }
-        document.querySelector('p').innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
-    }
-    recognition.start();
-    recognition.onspeechend = function() {
-        document.getElementById("load").className = document.getElementById("load").className.replace(/\bloading\b/,'');
-        document.getElementById("aura").style.animationName = "false";
-        document.getElementById("aura").style.background  = "transparent";
-        recognition.stop();
-    }
+      }, 1000);
 }
 
 timer();
