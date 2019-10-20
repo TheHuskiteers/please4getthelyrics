4650cc3a9b90cad75d125ba0dc465577d8d0a278
brandonmino <43628774+brandonmino@users.noreply.github.com>
2019-10-20 05:10:09 -0400

    Remove unused file and start haul to move all client html to one html

--- Client/js/joinstatus.js
+++
@@ -1,6 +0,0 @@
-var io = require('socket.io-client');
-var socket = io.connect('http://localhost:3000', {reconnect: true});
-
-socket.on('client join success', function () {
-    window.location.href = '../Client/html/waithost.html';
-  });
