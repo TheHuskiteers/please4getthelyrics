928a2c56477c6ab2d356beacc26e2eb0a9af0b70
brandonmino <43628774+brandonmino@users.noreply.github.com>
2019-10-20 00:52:04 -0400

    Fixed counter and getting glow fixed

--- Client/js/countdown.js
+++ Client/js/countdown.js
@@ -1,25 +1,48 @@
 function timer () {
   var timeleft = document.currentScript.getAttribute('time');
-  var status = document.currentScript.getAttribute('status');
   var downloadTimer = setInterval(function () {
   timeleft--
   document.getElementById('countdowntimer').textContent = timeleft
   if (timeleft <= 0) {
     clearInterval(downloadTimer)
+    mic();
   }
 }, 1000)
-}
+};
 
-function turn () {
-  setTimeout(function () {
-    var check = 1
-    var myturn = false
-    if (myturn == true) {
-      window.location.href = '../html/myturn.html'
-    } else {
-      window.location.href = '../html/waitturn.html'
+function mic() {
+    document.getElementById("load").className = document.getElementById("load").className.replace('',/\bloading\b/);
+    document.getElementById("aura").style.animationName = "true";
+    document.getElementById("aura").style.background  = "radial-gradient(#ffffff, transparent)";
+
+    window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+    let finalTranscript = '';
+    let recognition = new window.SpeechRecognition();
+    recognition.interimResults = true;
+    recognition.maxAlternatives = 10;
+    recognition.continuous = false;
+    recognition.onresult = (event) => {
+        document.getElementById("load").className = document.getElementById("load").className.replace('',/\bloading\b/);
+        document.getElementById("aura").style.animationName = "true";
+        document.getElementById("aura").style.background  = "radial-gradient(#ffffff, transparent)";
+        let interimTranscript = '';
+        for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+        let transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) {
+            finalTranscript += transcript;
+        } else {
+            interimTranscript += transcript;
+        }
+        }
+        document.querySelector('p').innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
     }
-  }, 1000)
+    recognition.start();
+    recognition.onspeechend = function() {
+        document.getElementById("load").className = document.getElementById("load").className.replace(/\bloading\b/,'');
+        document.getElementById("aura").style.animationName = "false";
+        document.getElementById("aura").style.background  = "transparent";
+        recognition.stop();
+    }
 }
 
 timer();
