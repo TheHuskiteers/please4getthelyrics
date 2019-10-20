9a9b6ab55a2a738ba452d11ee39155af0a5e7378
brandonmino <43628774+brandonmino@users.noreply.github.com>
2019-10-20 06:55:10 -0400

    Sweep sweep... (rip client folder ;( )

--- public/js/accessmic.js
+++
@@ -1,7 +0,0 @@
-navigator.mediaDevices.getUserMedia({ audio: true })
-      .then(function(stream) {
-        console.log('Access to mic allowed!')
-      })
-      .catch(function(err) {
-        console.log('Access to mic denied!')
-      });
